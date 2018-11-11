fc6041cc66a0c93249c97f1ae71922a3525acc78
Lynette Lopez <lynettelopez12@gmail.com>
2018-11-11 16:49:33 -0500

    Ready to pull

--- src/strategy/Group3.java
+++ src/strategy/Group3.java
@@ -1,7 +1,19 @@
 package strategy;
 
+import reversi.Board;
+import reversi.Square;
+import reversi.Strategy;
+
 public class Group3 implements Strategy {
 
+	@Override
+	public Square chooseSquare(Board board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
     
 
+	
 }
